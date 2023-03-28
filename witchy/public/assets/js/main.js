f97d6ae9cf2bf4a66f8934eab6040823cfb7f810
MaddoxGraham <57898176+MaddoxGraham@users.noreply.github.com>
2023-03-28 16:51:59 +0200

    finishedhead28/03/2023

--- witchy/public/assets/js/main.js
+++ witchy/public/assets/js/main.js
@@ -4,20 +4,27 @@ catBtn = document.querySelectorAll('.majorClass');
 
 for (let index = 0; index < catBtn.length; index++) {
     //const element = array[index];
-thiscatBtn = catBtn[index];
+    thiscatBtn = catBtn[index];
+    catUnder = document.querySelectorAll('.undernav');
+    
 
-thiscatBtn.addEventListener("click", function(){
+    thiscatBtn.addEventListener("click", function () {
 
-    subcat = document.getElementsByClassName('undernav')[index];
-    if (subcat.classList.contains('underHidden')){
-        subcat.classList.remove('underHidden');
-     
+        subcat = document.getElementsByClassName('undernav')[index];
+        catUnder.forEach(element => {
+           if(element == subcat){}else{
+            element.classList.add('underHidden');
+           }
+        });
 
-    }else{
-        subcat.classList.add('underHidden');
-        
-    }
+        if (subcat.classList.contains('underHidden')) {
+            subcat.classList.remove('underHidden');
+         } 
+        else {
+            subcat.classList.add('underHidden');
 
-})
+        }
+    })
 
 }
+
