e2e60c2ffd2a30638f196698e0d8ff4975d31116
MaddoxGraham <57898176+MaddoxGraham@users.noreply.github.com>
2023-04-07 11:43:58 +0200

    she

--- witchy/src/Entity/Client.php
+++ witchy/src/Entity/Client.php
@@ -59,6 +59,12 @@ class Client implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\OneToMany(mappedBy: 'idClient', targetEntity: Historique::class, orphanRemoval: true)]
     private Collection $historiques;
 
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $profPic = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $profBan = null;
+
     public function __construct()
     {
         $this->adresses = new ArrayCollection();
@@ -276,6 +282,30 @@ class Client implements UserInterface, PasswordAuthenticatedUserInterface
                 $historique->setIdClient(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getProfPic(): ?string
+    {
+        return $this->profPic;
+    }
+
+    public function setProfPic(?string $profPic): self
+    {
+        $this->profPic = $profPic;
+
+        return $this;
+    }
+
+    public function getProfBan(): ?string
+    {
+        return $this->profBan;
+    }
+
+    public function setProfBan(?string $profBan): self
+    {
+        $this->profBan = $profBan;
 
         return $this;
     }
