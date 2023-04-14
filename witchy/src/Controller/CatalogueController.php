3e990947cbd32913709231f2424dfe8d0c134df9
MaddoxGraham <57898176+MaddoxGraham@users.noreply.github.com>
2023-04-14 11:45:11 +0200

    14/04/2023_RDConstructRecuperationData

--- witchy/src/Controller/CatalogueController.php
+++ witchy/src/Controller/CatalogueController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategorieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class CatalogueController extends AbstractController
 {
     #[Route('/', name: 'app_catalogue')]
-    public function index(): Response
+    public function index(CategorieRepository $categorieRepository): Response
     {
         return $this->render('catalogue/index.html.twig', [
-            'controller_name' => 'CatalogueController',
+            'categories' => $categorieRepository->findBy([],['id' => 'asc']),
         ]);
     }
+    // public function sousCategories(CategorieRepository $categorieRepository):Response
+    // {
+    //     for ($souscat=1; $souscat  ; $souscat++) { 
+    //         # code...
+    //     }
+    // }
+
+
 }
